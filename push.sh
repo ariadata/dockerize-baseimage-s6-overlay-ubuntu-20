9652dd43649a860d5af3e4e761cbf0c045e3fddc
Mehrdad Amini <mehrdad@arzinja.dev>
2025-07-02 18:58:23 +0300

    update baseimage to jammy

--- push.sh
+++ push.sh
@@ -3,6 +3,7 @@
 # exit when any command fails
 set -e
 
-docker push ariadata/baseimage-s6-overlay-ubuntu-20:v2
+docker push ariadata/baseimage-s6-overlay-ubuntu-20:jammy
+docker tag ariadata/baseimage-s6-overlay-ubuntu-20:jammy ariadata/baseimage-s6-overlay-ubuntu-20:latest
 
 echo "\n🔥 Pushing to dockerhub is Done!"
